c3ab8140e11b8d15578d52c9267a1e86c4851c1d
Evgeny <arswarog@yandex.ru>
2021-07-22 22:28:08 +0300

    fix(todolist): add new todo into selected list

--- examples/todolist/src/hooks/useTodoOperations.tsx
+++ examples/todolist/src/hooks/useTodoOperations.tsx
@@ -2,19 +2,19 @@ import { makeSpaceID } from '@chunk-db/core';
 import { useChunkDB } from '@chunk-db/react';
 import { useCallback } from 'react';
 
-import { ITodo, makeListID, makeTodoID, todoScheme } from '../store/store.types';
+import { ITodo, ListID, makeTodoID, todoScheme } from '../store/store.types';
 
 export function useAddTodo() {
     const db = useChunkDB();
 
     return useCallback(
-        (title: string) => {
-            const trimmedText = title.trim();
-            if (!trimmedText) return;
+        (title: string, listId: ListID) => {
+            title = title.trim();
+            if (!title) return;
             const todo: ITodo = {
                 _id: makeTodoID(Math.random().toString(16).substring(2)),
-                title: trimmedText,
-                listId: makeListID(''),
+                title,
+                listId,
             };
             db.transaction(makeSpaceID('space'), tx => {
                 return tx.upsert(todoScheme, todo);
